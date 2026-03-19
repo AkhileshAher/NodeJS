9dab240e8c64441c6ef354529437d448514f6188
Akhilesh Aher <aherakhilesh88@gmail.com>
2026-03-19 23:44:41 +0530

    Modules in NodeJS

---
+++ 1_Modules/app.js
@@ -0,0 +1,10 @@
+// global.console.log("Akhilesh Aher");
+// console.log(globalThis);
+
+// globalThis.console.log(module);
+
+// console.log(process);
+
+import add from './math.js';
+
+add(12,12);
